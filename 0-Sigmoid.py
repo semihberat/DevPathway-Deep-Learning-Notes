c8f2f50397b5d0682726ad2124234f178c2c2e12
semihberat <dev.berat@outlook.com>
2024-07-30 21:21:29 +0300

    Update 0-Sigmoid.py

--- 0-Sigmoid.py
+++ 0-Sigmoid.py
@@ -2,3 +2,5 @@ import numpy as np
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
+""" Girilen her değer 0 ile 1 arasında bir sayı döndürecektir. Bu da sayılar arasında bir korelasyon oluşmasına 
+ve istatistiksel bir çıkarım yapmaya elverişli hale getirir. """
